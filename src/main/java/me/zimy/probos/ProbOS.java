898977159097ee701db2768e644255e22c8531ec
Dmitriy Zimy Yakovlev <zimy@yandex.ru>
2014-12-08 01:43:43 +0300

    Initial commit

---
+++ src/main/java/me/zimy/probos/ProbOS.java
@@ -0,0 +1,34 @@
+package me.zimy.probos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+/**
+ * @author Dmitriy &lt;Zimy&gt; Yakovlev
+ * @since 12/1/14.
+ */
+@EntityScan
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class ProbOS {
+    public static void main(String[] args) {
+        SpringApplication.run(ProbOS.class, args);
+    }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        return filter;
+    }
+}
