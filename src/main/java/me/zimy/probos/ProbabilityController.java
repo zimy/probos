48b03148daf485e9a35dd0c8fd78ebe9cd14d186
Dmitriy Zimy Yakovlev <zimy@yandex.ru>
2014-12-11 07:36:55 +0300

    Now receiving statistics made FAST with total precalculation and recalculation on demand.

--- src/main/java/me/zimy/probos/ProbabilityController.java
+++ src/main/java/me/zimy/probos/ProbabilityController.java
@@ -1,17 +1,16 @@
 package me.zimy.probos;
 
-import me.zimy.probos.services.MagicService;
-import me.zimy.probos.services.SecondResult;
-import me.zimy.probos.services.SortingCalculationSettings;
+import me.zimy.probos.services.PersistentStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 
 /**
+ * Main and only HTTP controller
+ *
  * @author Dmitriy &lt;Zimy&gt; Yakovlev
  * @since 12/9/14.
  */
@@ -20,27 +19,16 @@ import java.util.concurrent.Future;
 public class ProbabilityController {
 
     @Autowired
-    private MagicService magicService;
-
-    @Autowired
-    private SortingCalculationSettings sortingCalculationSettings;
+    PersistentStorage persistentStorage;
 
     @RequestMapping("/")
     public String getProbabilityResults(Model model) throws InterruptedException, ExecutionException {
-        Future<Integer> integerFuture = magicService.calculateNormalDistribution(400, 0.9, 1000, 0, 1);
-        Future<Integer> integerFuture1 = magicService.calculateNormalDistribution(10, 0.9, 1000, 0, 1);
-        Future<Integer> integerFuture2 = magicService.calculateMyDistribution(400, 0.9, 1000, 0.5, 3, -3);
-        Future<Integer> integerFuture3 = magicService.calculateMyDistribution(10, 0.9, 1000, 0.5, 3, -3);
-        Future<SecondResult> calculate = sortingCalculationSettings.calculate();
-        while (!(integerFuture.isDone() && integerFuture2.isDone() && integerFuture1.isDone() && integerFuture3.isDone() && calculate.isDone())) {
-            Thread.sleep(20);
-        }
-
-        model.addAttribute("my400", integerFuture2.get());
-        model.addAttribute("my10", integerFuture3.get());
-        model.addAttribute("n400", integerFuture.get());
-        model.addAttribute("n10", integerFuture1.get());
-        model.addAttribute("secondResult", calculate.get());
+        model.addAttribute("my400", persistentStorage.getM400());
+        model.addAttribute("my10", persistentStorage.getM10());
+        model.addAttribute("n400", persistentStorage.getN400());
+        model.addAttribute("n10", persistentStorage.getN10());
+        model.addAttribute("secondResult", persistentStorage.getSecondResult());
+        persistentStorage.recalculate();
         return "answers";
     }
 }
---
+++ src/main/java/me/zimy/probos/services/PersistentStorage.java
@@ -0,0 +1,73 @@
+package me.zimy.probos.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+/**
+ * This is storage for making statistics FAST.
+ * It if firstly filled on startup time, and then is re-generated on every http-request.
+ *
+ * @author Dmitriy &lt;Zimy&gt; Yakovlev
+ * @since 12/11/14.
+ */
+@Service
+public class PersistentStorage {
+    Integer m400 = 0, m10 = 0, n400 = 0, n10 = 0;
+    SecondResult secondResult = new SecondResult();
+
+    @Autowired
+    private MagicService magicService;
+    @Autowired
+    private SortingCalculationSettings sortingCalculationSettings;
+
+    @Async
+    public void recalculate() throws ExecutionException, InterruptedException {
+        Future<Integer> fn400 = magicService.calculateNormalDistribution(400, 0.9, 1000, 0, 1);
+        Future<Integer> fn10 = magicService.calculateNormalDistribution(10, 0.9, 1000, 0, 1);
+        Future<Integer> fm400 = magicService.calculateMyDistribution(400, 0.9, 1000, 0.5, 3, -3);
+        Future<Integer> fm10 = magicService.calculateMyDistribution(10, 0.9, 1000, 0.5, 3, -3);
+        Future<SecondResult> stats = sortingCalculationSettings.calculate();
+        while (!(fn400.isDone() && fm400.isDone() && fn10.isDone() && fm10.isDone() && stats.isDone())) {
+            Thread.sleep(20);
+        }
+        m400 = fm400.get();
+        n400 = fn400.get();
+        m10 = fm10.get();
+        n10 = fn10.get();
+        secondResult = stats.get();
+    }
+
+    public Integer getM400() {
+        return m400;
+    }
+
+    public Integer getM10() {
+        return m10;
+    }
+
+    public Integer getN400() {
+        return n400;
+    }
+
+    public Integer getN10() {
+        return n10;
+    }
+
+    public SecondResult getSecondResult() {
+        return secondResult;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        try {
+            recalculate();
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
